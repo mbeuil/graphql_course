4f29d15dd49eba1cfb4a9ea43c97ffbdb5192e70
Maxime Beuil <mbeuil@MBP-de-Maxime.localdomain>
2021-10-26 15:05:24 +0200

    appolo server finalize

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,7 +1,48 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  # Your schema will go here
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): User
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
   type Launch {
     id: ID!
     site: String
@@ -9,39 +50,29 @@ const typeDefs = gql`
     rocket: Rocket
     isBooked: Boolean!
   }
+
   type Rocket {
     id: ID!
     name: String
     type: String
   }
+
   type User {
     id: ID!
     email: String!
+    profileImage: String
     trips: [Launch]!
     token: String
   }
+
   type Mission {
     name: String
     missionPatch(size: PatchSize): String
   }
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launches: [Launch]
-  }
+
   enum PatchSize {
     SMALL
     LARGE
-  }
-  type Query {
-    launches: [Launch]!
-    launch(id: ID!): Launch
-    me: User
-  }
-  type Mutation {
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-    login(email: String): User
   }
 `;
 
